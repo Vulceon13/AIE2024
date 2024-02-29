5a3a2dbcccb892b0d194451d0523a23843160925
Deion Trinidad <deion_trinidad@outlook.com>
2024-02-29 22:24:18 +1100

    Starting to create some functions for my Custom Utility Class

--- IntrocPP_Task1_StringUtilityClass/IntrocPP_Task1_StringUtilityClass/String.cpp
+++ IntrocPP_Task1_StringUtilityClass/IntrocPP_Task1_StringUtilityClass/String.cpp
@@ -1,6 +1,23 @@
 #include "String.h"
+#include "iostream"
+using namespace std;
 
 String::String() 
+{
+	
+}
+
+String::String(const char* _str) 
+{
+
+}
+
+String::String(const String& _other)
+{
+
+}
+
+String::~String() 
 {
 
 }
