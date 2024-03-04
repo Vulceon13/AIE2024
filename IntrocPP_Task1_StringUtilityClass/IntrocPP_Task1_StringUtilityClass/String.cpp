a99bf0d5646f3e1b8e6c0614a487744beebd5ca9
Deion Trinidad <deion_trinidad@outlook.com>
2024-03-04 17:06:17 +1100

    Adding more comments

--- IntrocPP_Task1_StringUtilityClass/IntrocPP_Task1_StringUtilityClass/String.cpp
+++ IntrocPP_Task1_StringUtilityClass/IntrocPP_Task1_StringUtilityClass/String.cpp
@@ -50,76 +50,91 @@ bool String::EqualTo(const String& _other) const
 
 }
 
+// Adding a string to another string
 String& String::Append(const String& _str) 
 {
 
 }
 
+// Adding a string to the beginning of another string 
 String& String::Prepend(const String& _str) 
 {
 
 }
 
+// Converts string to a C-Style String
 const char* String::CStr() const 
 {
 
 }
 
+// Converts the string to all lowercase
 String& String::ToLower() 
 {
 
 }
 
+// Converts the string to all uppercase
 String& String::ToUpper()
 {
 
 }
 
+// Function to find a substring
 size_t String::Find(const String& _str) 
 {
 
 }
 
+// Function to find a substring with a given index
 size_t String::Find(size_t _startIndex, const String& _str) 
 {
 
 }
 
+// Function to replace one substring with another string 
 String& String::Replace(const String& _find, const String& _replace) 
 {
 
 }
 
+// Function to read the console
 String& String::ReadFromConsole() 
 {
 
 }
 
+// Function to wrtie to the console
 String& String::WriteToConsole() 
 {
 
 }
 
+// 
 bool String::operator==(const String& _other) 
 {
 
 }
 
+// 
 bool String::operator!=(const String& _other) 
 {
 
 }
 
+// 
 String& String::operator=(const String& _str) 
 {
 
 }
 
+// 
 char& String::operator[](size_t _index) 
 { 
 
 }
 
+// 
 const char& String::operator[](size_t _index) const 
 {
 
